22a97e7d934712f6d1a9e5a9c7b4ed4840aee897
DenissiusMaxximus <deniskramar07@gmail.com>
2024-11-18 21:44:09 +0200

    PointsAverage

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int[] grades = new int[5];
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Введіть оцінку студента: ");
+            grades[i] = int.Parse(Console.ReadLine());
+            sum += grades[i];
+        }
+
+        double average = sum / 5.0;
+
+        if (average >= 50)
+        {
+            Console.WriteLine("Студент допущений до екзамену.");
+        }
+        else
+        {
+            Console.WriteLine("Студент не допущений до екзамену.");
+        }
+    }
+}
